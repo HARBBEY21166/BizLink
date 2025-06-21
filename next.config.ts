961d290c2f3279229b9e0f94f980071846927bdc
ABBEY AINA <olamilekansunday445@gmail.com>
2025-06-21 14:02:57 +0000

    this app displays differently after i deployed on vercel

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   // No srcDir needed if 'src' is at the root and contains 'app'
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
